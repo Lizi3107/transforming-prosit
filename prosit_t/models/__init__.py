bd9466e2477c49e5c5ccddb4e96f813012200b87
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-06-02 18:49:45 +0200

    add inits

--- prosit_t/models/__init__.py
+++ prosit_t/models/__init__.py
@@ -1,5 +1,2 @@
 from prosit_t.models.prosit import PrositIntensityPredictor
 from prosit_t.models.prosit_transformer import PrositTransformerIntensityPredictor
-from prosit_t.models.prosit_transformer_factor import (
-    PrositTransformerIntensityPredictorV2,
-)
