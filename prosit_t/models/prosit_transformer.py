d25112eacac5a339a6884e71fbaab1d4b2f6daa3
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-06-09 10:47:14 +0200

    add run name to train script

--- prosit_t/models/prosit_transformer.py
+++ prosit_t/models/prosit_transformer.py
@@ -7,7 +7,9 @@ from prosit_t.layers import (
     FusionLayer,
     TransformerDecoder,
     Regressor,
-    SequenceEncoderNoGRU,
+    RegressorV2,
+    TransformerEncoder,
+    PositionalEmbedding,
 )
 
 
@@ -27,6 +29,7 @@ class PrositTransformerIntensityPredictor(tf.keras.Model):
         regressor_layer_size=512,
         layer_norm_epsilon=1e-5,
         num_encoders=5,
+        **kwargs
     ):
         super(PrositTransformerIntensityPredictor, self).__init__()
 
@@ -42,15 +45,13 @@ class PrositTransformerIntensityPredictor(tf.keras.Model):
             vocabulary=list(vocab_dict.keys())
         )
 
-        self.embedding = tf.keras.layers.Embedding(
-            input_dim=self.embeddings_count,
-            output_dim=embedding_output_dim,
-            input_length=seq_length,
+        self.pos_embedding = PositionalEmbedding(
+            self.embeddings_count, embedding_output_dim
         )
 
         self.attention = AttentionLayer(name="encoder_att")
         self.fusion_layer = FusionLayer(self.max_ion)
-        self.sequence_encoder = SequenceEncoderNoGRU(
+        self.sequence_encoder = TransformerEncoder(
             intermediate_dim,
             transformer_num_heads,
             mh_num_heads,
@@ -72,10 +73,13 @@ class PrositTransformerIntensityPredictor(tf.keras.Model):
             self.max_ion,
             num_encoders,
         )
-        self.regressor = Regressor(len_fion)
+
+        # self.flatten = tf.keras.layers.Flatten()
+        self.extract_last_token = tf.keras.layers.Lambda(lambda x: x[:, -1, :])
+
+        self.regressor = RegressorV2(174)
 
     def summary(self):
-        print("------------------")
         in_sequence = tf.keras.layers.Input(shape=(30,))
         in_collision_energy = tf.keras.layers.Input(shape=(1,))
         in_precursor_charge = tf.keras.layers.Input(shape=(6,))
@@ -95,13 +99,14 @@ class PrositTransformerIntensityPredictor(tf.keras.Model):
         peptides_in = inputs["sequence"]
         collision_energy_in = inputs["collision_energy"]
         precursor_charge_in = inputs["precursor_charge"]
-        print(collision_energy_in.shape, precursor_charge_in.shape)
         encoded_meta = self.meta_encoder([collision_energy_in, precursor_charge_in])
         x = self.string_lookup(peptides_in)
-        x = self.embedding(x)
+        x = self.pos_embedding(x)
         x = self.attention(x)
         x = self.fusion_layer([x, encoded_meta])
         x = self.sequence_encoder(x)
         x = self.decoder(x)
+        # x = self.flatten(x)
+        x = self.extract_last_token(x)
         x = self.regressor(x)
         return x
