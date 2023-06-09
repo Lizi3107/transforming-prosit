bce91cba035bb266dfde1110b943e774c046aa33
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-06-09 10:43:45 +0200

    add attention layers in init

--- prosit_t/layers/__init__.py
+++ prosit_t/layers/__init__.py
@@ -1,10 +1,11 @@
-from prosit_t.attention_layers import BaseAttention, CrossAttention, GlobalSelfAttention, CausalSelfAttention, FeedForward
+from prosit_t.layers.attention_layers import BaseAttention, CrossAttention, GlobalSelfAttention, CausalSelfAttention, FeedForward
 from prosit_t.layers.fusion_layer import FusionLayer
 from prosit_t.layers.gru_decoder import GRUDecoder
 from prosit_t.layers.meta_encoder import MetaEncoder
 from prosit_t.layers.positional_embedding import PositionalEmbedding
 from prosit_t.layers.regressor_time_distributed import RegressorTimeDistributed
 from prosit_t.layers.regressor import Regressor
+from prosit_t.layers.regressor_v2 import RegressorV2
 from prosit_t.layers.sequence_encoder_transformer_gru import SequenceEncoderTransformerGRU
 from prosit_t.layers.transformer_encoder import TransformerEncoder
 from prosit_t.layers.transformer_decoder import TransformerDecoder
