dcd65ed8a1a62b59b94b112eb5cf13878d79268b
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-07-03 16:08:18 +0200

    cleantrain pipeline, import utils

--- prosit_t/wandb_agent/train_simple_model.py
+++ prosit_t/wandb_agent/train_simple_model.py
@@ -4,11 +4,10 @@ import wandb
 from wandb.keras import WandbCallback
 from prosit_t.models import PrositSimpleIntensityPredictor
 from dlomix.losses import masked_spectral_distance, masked_pearson_correlation_distance
-from dlomix.data import IntensityDataset
 from dlomix.constants import ALPHABET_UNMOD
+from train_utils import get_example_data, get_proteometools_data
 import os
 
-TRAIN_DATAPATH = "https://raw.githubusercontent.com/wilhelm-lab/dlomix-resources/main/example_datasets/Intensity/proteomeTools_train_val.csv"
 PROJECT_NAME = "transforming-prosit"
 EPOCHS = 60
 
@@ -19,9 +18,10 @@ DEFAULT_CONFIG = {
     "seq_length": 30,
     "len_fion": 6,
     "vocab_dict": ALPHABET_UNMOD,
-    "dropout_rate": 0.1,
+    "dropout_rate": 0,
     "ff_dim": 32,
     "num_heads": 16,
+    "transformer_dropout": 0.1,
 }
 
 
@@ -46,25 +46,17 @@ def get_callbacks(config):
     return callbacks
 
 
-def get_data(run, config):
-    BATCH_SIZE = config["batch_size"]
-    int_data = IntensityDataset(
-        data_source=TRAIN_DATAPATH,
-        seq_length=config["seq_length"],
-        collision_energy_col="collision_energy",
-        batch_size=BATCH_SIZE,
-        val_ratio=0.2,
-        test=False,
-    )
-    return int_data.train_data, int_data.val_data
-
-
 def train(config=None):
     with wandb.init(config=config, project=PROJECT_NAME) as run:
         config = wandb.config
         config = dict(wandb.config)
 
-        train_dataset, val_dataset = get_data(run, config)
+        if config["dataset"] == "example":
+            train_dataset, val_dataset = get_example_data(run, config)
+        else:
+            assert "dataset_source" in config
+            train_dataset, val_dataset = get_proteometools_data(run, config)
+
         model = get_model(config)
         callbacks = get_callbacks(config)
         model.fit(
