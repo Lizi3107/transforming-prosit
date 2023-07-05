31727a7df2dc1ce548af6b109b3c062d0589980b
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-07-05 13:10:46 +0200

    add the pipleine for training prosit

--- prosit_t/wandb_agent/train_prosit.py
+++ prosit_t/wandb_agent/train_prosit.py
@@ -1,35 +1,35 @@
 import tensorflow as tf
 import wandb
 from wandb.keras import WandbCallback
-from prosit_t.models import PrositIntensityPredictor
+from dlomix.models import PrositIntensityPredictor
 from dlomix.losses import masked_spectral_distance, masked_pearson_correlation_distance
-from dlomix.data import IntensityDataset
 from dlomix.constants import ALPHABET_UNMOD
+from prosit_t.wandb_agent import train_utils
 
-TRAIN_DATAPATH = "https://raw.githubusercontent.com/wilhelm-lab/dlomix-resources/main/example_datasets/Intensity/proteomeTools_train_val.csv"
 PROJECT_NAME = "transforming-prosit"
-EPOCHS = 60
+EPOCHS = 200
 DEFAULT_CONFIG = {
-    "name": "baseline_hcd_third_pool",
     "learning_rate": 0.0001,
     "batch_size": 64,
     "embedding_output_dim": 16,
-    "intermediate_dim": 512,
-    "transformer_num_heads": 4,
-    "mh_num_heads": 4,
-    "key_dim": 4,
     "seq_length": 30,
     "len_fion": 6,
     "vocab_dict": ALPHABET_UNMOD,
-    "dropout_rate": 0.2,
-    "latent_dropout_rate": 0.1,
-    "layer_norm_epsilon": 1e-5,
+    "recurrent_layers_sizes": (256, 512),
     "regressor_layer_size": 512,
+    "dataset": "proteometools",
+    "data_source": "/cmnfs/home/l.mamisashvili/transforming-prosit/notebooks/input_config.json",
+    "fragmentation": "HCD",
+    "mass_analyzer": "FTMS",
 }
 
 
 def get_model(config):
-    model = PrositIntensityPredictor()
+    model = PrositIntensityPredictor(
+        seq_length=config["seq_length"],
+        embedding_output_dim=config["embedding_output_dim"],
+        recurrent_layers_sizes=config["recurrent_layers_sizes"],
+    )
     model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=config["learning_rate"]),
         loss=masked_spectral_distance,
@@ -45,25 +45,12 @@ def get_callbacks(config):
     return callbacks
 
 
-def get_data(run, config):
-    BATCH_SIZE = config["batch_size"]
-    int_data = IntensityDataset(
-        data_source=TRAIN_DATAPATH,
-        seq_length=config["seq_length"],
-        collision_energy_col="collision_energy",
-        batch_size=BATCH_SIZE,
-        val_ratio=0.2,
-        test=False,
-    )
-    return int_data.train_data, int_data.val_data
-
-
 def train(config=None):
     with wandb.init(config=config, project=PROJECT_NAME) as run:
         config = wandb.config
         config = dict(wandb.config)
 
-        train_dataset, val_dataset = get_data(run, config)
+        train_dataset, val_dataset = train_utils.get_proteometools_data(config)
         model = get_model(config)
         callbacks = get_callbacks(config)
         model.fit(
