c0d7c81cda6ffb531f01dd0b508f0248b6566d06
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-07-05 18:21:07 +0200

    add transformer encoder

--- prosit_t/layers/transformer.py
+++ prosit_t/layers/transformer.py
@@ -25,3 +25,25 @@ class TransformerBlock(tf.keras.layers.Layer):
         ffn_output = self.ffn(out1)
         ffn_output = self.dropout2(ffn_output, training=training)
         return self.layernorm2(out1 + ffn_output)
+
+
+class TransformerEncoder(tf.keras.layers.Layer):
+    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1, num_encoders=2):
+        super().__init__()
+        self.num_encoders = num_encoders
+        self.transformer_blocks = [
+            TransformerBlock(
+                embed_dim=embed_dim,
+                num_heads=num_heads,
+                ff_dim=ff_dim,
+                rate=rate,
+                num_encoders=num_encoders,
+            )
+            for _ in range(num_encoders)
+        ]
+
+    def call(self, x, **kwargs):
+        for i in range(self.num_encoders):
+            x = self.transformer_blocks[i](x)
+
+        return x
