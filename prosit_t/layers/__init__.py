d40465343d0333afd29a78420aed8e2fc7da0327
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-07-05 18:20:38 +0200

    add transformer encoder to the layers

--- prosit_t/layers/__init__.py
+++ prosit_t/layers/__init__.py
@@ -7,8 +7,9 @@ from prosit_t.layers.regressor_time_distributed import RegressorTimeDistributed
 from prosit_t.layers.regressor import Regressor
 from prosit_t.layers.regressor_v2 import RegressorV2
 from prosit_t.layers.sequence_encoder_transformer_gru import SequenceEncoderTransformerGRU
-from prosit_t.layers.transformer_encoder import TransformerEncoder
+# from prosit_t.layers.transformer_encoder import TransformerEncoder
 from prosit_t.layers.transformer_decoder import TransformerDecoder
 from prosit_t.layers.sequence_context import Encoder, DecoderMeta, MetaEmbeddingSimple
 from prosit_t.layers.gru_encoder import GRUEncoder
 from prosit_t.layers.transformer import TransformerBlock
+from prosit_t.layers.transformer import TransformerEncoder
