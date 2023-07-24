77d0a71074a51163ebf79cc1d5cf7d82d551a6cc
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-07-24 18:30:58 +0200

    context in init

--- prosit_t/models/__init__.py
+++ prosit_t/models/__init__.py
@@ -1,5 +1,10 @@
 from prosit_t.models.prosit import PrositIntensityPredictor
 from prosit_t.models.prosit_transformer import PrositTransformerIntensityPredictor
-from prosit_t.models.prosit_transformer_meta_context import PrositTransformerWithMetaContextIntensityPredictor
+from prosit_t.models.prosit_transformer_meta_context import (
+    PrositTransformerWithMetaContextIntensityPredictor,
+)
 from prosit_t.models.prosit_no_gru import PrositNoGRUIntensityPredictor
 from prosit_t.models.prosit_simple_transformer import PrositSimpleIntensityPredictor
+from prosit_t.models.prosit_simple_transformer_with_meta_context import (
+    PrositMetaContextIntensityPredictor,
+)
