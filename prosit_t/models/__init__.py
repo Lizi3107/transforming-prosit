6897c0e79bc9c7c31979b893ec4452fc181c3dd7
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-08-02 16:03:34 +0200

    remove archived models from init

--- prosit_t/models/__init__.py
+++ prosit_t/models/__init__.py
@@ -1,10 +1,6 @@
-from prosit_t.models.prosit import PrositIntensityPredictor
-from prosit_t.models.prosit_transformer import PrositTransformerIntensityPredictor
+from prosit_t.models.prosit_transformer import PrositTransformer
 from prosit_t.models.prosit_transformer_meta_context import (
-    PrositTransformerWithMetaContextIntensityPredictor,
+    PrositTransformerMetaContext,
 )
-from prosit_t.models.prosit_no_gru import PrositNoGRUIntensityPredictor
-from prosit_t.models.prosit_simple_transformer import PrositSimpleIntensityPredictor
-from prosit_t.models.prosit_simple_transformer_with_meta_context import (
-    PrositMetaContextIntensityPredictor,
-)
+from prosit_t.models.prosit_transformer_fusion_up import PrositTransformerFusionUp
+from prosit_t.models.prosit_transformer_enc_dec import PrositTransformerEncDec
