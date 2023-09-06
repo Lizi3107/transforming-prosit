8fac32404fb2523a0017d7559bbf59a7352591ca
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-09-06 14:29:13 +0200

    clean train pipeline for v2 model

--- prosit_t/wandb_agent/train_transformer_model_v2.py
+++ prosit_t/wandb_agent/train_transformer_model_v2.py
@@ -3,9 +3,6 @@ import tensorflow as tf
 from dlomix.losses import masked_spectral_distance
 from dlomix.constants import ALPHABET_UNMOD
 from prosit_t.wandb_agent.train_utils import train
-import os
-
-PROJECT_NAME = "transforming-prosit"
 
 DEFAULT_CONFIG = {
     "learning_rate": 0.0001,
@@ -14,7 +11,7 @@ DEFAULT_CONFIG = {
     "seq_length": 30,
     "len_fion": 6,
     "vocab_dict": ALPHABET_UNMOD,
-    "dropout_rate": 0.2,
+    "dropout_rate": 0,
     "ff_dim": 32,
     "num_heads": 16,
     "transformer_dropout": 0.1,
@@ -23,23 +20,11 @@ DEFAULT_CONFIG = {
         "train": "/cmnfs/proj/prosit/Transformer/first_pool_train.parquet",
         "val": "/cmnfs/proj/prosit/Transformer/first_pool_test.parquet",
     },
-    "fragmentation": "HCD",
     "early_stopping": {
         "patience": 8,
         "min_delta": 0.0001,
     },
-    "cyclic_lr": {
-        "base_lr": 0.00001,
-        "gamma": 0.95,
-        "max_lr": 0.0002,
-        "mode": "triangular",
-        "step_size": 4,
-    },
-    # "reduce_lr": {
-    #     "factor": 0.5,
-    #     "patience": 4
-    # },
-    # "lr_scheduler": True,
+    "reduce_lr": {"factor": 0.5, "patience": 4},
     "epochs": 500,
     "num_transformers": 6,
     "dense_dim_factor": 4,
@@ -61,7 +46,7 @@ def get_model(config):
 
 
 def main():
-    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "2"
     physical_devices = tf.config.list_physical_devices("GPU")
     tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
     train(DEFAULT_CONFIG, get_model)
