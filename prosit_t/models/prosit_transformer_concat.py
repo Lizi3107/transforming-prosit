850d109485585a47f6b3c624b2bb744d29ac0b06
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-09-12 13:22:15 +0200

    remove mul add concat in fusion layer

---
+++ prosit_t/models/prosit_transformer_concat.py
@@ -0,0 +1,87 @@
+import tensorflow as tf
+from tensorflow.keras.layers.experimental import preprocessing
+from dlomix.constants import ALPHABET_UNMOD
+from prosit_t.layers import (
+    MetaEncoder,
+    RegressorV2,
+    PositionalEmbedding,
+    TransformerEncoder,
+)
+
+
+class PrositTransformerConcat(tf.keras.Model):
+    def __init__(
+        self,
+        embedding_output_dim=16,
+        seq_length=30,
+        len_fion=6,
+        vocab_dict=ALPHABET_UNMOD,
+        dropout_rate=0.2,
+        num_heads=8,
+        ff_dim=32,
+        transformer_dropout=0.1,
+        num_transformers=2,
+        dense_dim_factor=4,
+        **kwargs,
+    ):
+        super(PrositTransformerConcat, self).__init__()
+
+        # tie the count of embeddings to the size of the vocabulary (count of aa)
+        self.embeddings_count = len(vocab_dict) + 2
+
+        # maximum number of fragment ions
+        self.max_ion = seq_length - 1
+
+        self.string_lookup = preprocessing.StringLookup(
+            vocabulary=list(vocab_dict.keys())
+        )
+        self.pos_embedding = PositionalEmbedding(
+            self.embeddings_count, embedding_output_dim
+        )
+        self.meta_encoder = MetaEncoder(
+            embedding_output_dim * dense_dim_factor, dropout_rate
+        )
+
+        self.transformer_encoder = TransformerEncoder(
+            embedding_output_dim,
+            num_heads,
+            ff_dim,
+            rate=transformer_dropout,
+            num_transformers=num_transformers,
+        )
+
+        self.flatten = tf.keras.layers.Flatten()
+        self.concat = tf.keras.layers.Concatenate()
+        self.dense = tf.keras.layers.Dense(512)
+        self.regressor = RegressorV2(len_fion * self.max_ion)
+
+    def summary(self):
+        in_sequence = tf.keras.layers.Input(shape=(30,))
+        in_collision_energy = tf.keras.layers.Input(shape=(1,))
+        in_precursor_charge = tf.keras.layers.Input(shape=(6,))
+        outputs = self.call(
+            {
+                "sequence": in_sequence,
+                "collision_energy": in_collision_energy,
+                "precursor_charge": in_precursor_charge,
+            }
+        )
+        return tf.keras.Model(
+            inputs=[in_sequence, in_collision_energy, in_precursor_charge],
+            outputs=outputs,
+        ).summary()
+
+    def call(self, inputs, **kwargs):
+        peptides_in = inputs["sequence"]
+        collision_energy_in = inputs["collision_energy"]
+        precursor_charge_in = inputs["precursor_charge"]
+
+        encoded_meta = self.meta_encoder([collision_energy_in, precursor_charge_in])
+        x = self.string_lookup(peptides_in)
+        x = self.pos_embedding(x)
+        x = self.transformer_encoder(x)
+        x = self.flatten(x)
+        x = self.concat([x, encoded_meta])
+        x = self.dense(x)
+        x = self.regressor(x)
+        return x
