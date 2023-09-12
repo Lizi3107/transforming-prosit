fb5f38284b106dd5c920a90da8fee7daea2fab23
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-09-12 13:02:09 +0200

    clear regressors

--- prosit_t/layers/regressor_v2.py
+++ prosit_t/layers/regressor_v2.py
@@ -8,12 +8,8 @@ class RegressorV2(tf.keras.layers.Layer):
     ):
         super(RegressorV2, self).__init__()
         self.dense = tf.keras.layers.Dense(output_shape, name="dense")
-        # self.relu = tf.keras.layers.ReLU(name="activation")
-        # self.flatten = tf.keras.layers.Flatten(name="out")
 
     def call(self, inputs, **kwargs):
         x = self.dense(inputs)
-        # x = self.relu(x)
-        # x = self.flatten(x)
 
         return x
