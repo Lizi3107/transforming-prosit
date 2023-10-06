8a9ed6342b2085f8731f47a493c895135338cef6
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-10-06 19:58:03 +0200

    add a scriot for converting parquet files into tf datasets

---
+++ prosit_t/data/parquet_to_tfdataset.py
@@ -0,0 +1,141 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+pd.set_option("mode.chained_assignment", None)
+
+DATA_CONFIG = {
+    "dataset": "proteometools",
+    "data_source": {
+        "train": "/cmnfs/proj/prosit/Transformer/first_pool_train.parquet",
+        "val": "/cmnfs/proj/prosit/Transformer/first_pool_test.parquet",
+    },
+    "fragmentation": "HCD",
+    "batch_size": 1,
+    "seq_length": 30,
+}
+
+X_COLUMNS = ["sequence", "precursor_charge", "collision_energy"]
+MOD_ENCODING = ["[UNIMOD:35]", "[UNIMOD:4]"]
+
+
+def concatenate_columns(row):
+    return row.tolist()
+
+
+def truncate_target(row):
+    sequence = row["sequence"]
+    target = row["intensities_raw"][: (len(sequence) - 1) * 6]
+    return target
+
+
+def ragged_to_dense(x, y):
+    return x, y
+
+
+def merge_tuples(item1, item2):
+    return (
+        {
+            "sequence": item1[0],
+            "precursor_charge": item2[0],
+            "collision_energy": item2[1],
+        },
+        item1[-1],
+    )
+
+
+def create_input_df(parquet_df):
+    df = pd.DataFrame(columns=X_COLUMNS)
+    df["sequence"] = (
+        parquet_df["modified_sequence"]
+        .str.replace(MOD_ENCODING[0], "", regex=False)
+        .str.replace(MOD_ENCODING[1], "", regex=False)
+    )
+    df["precursor_charge"] = pd.get_dummies(
+        parquet_df["precursor_charge"], dtype=float
+    ).apply(lambda row: concatenate_columns(row), axis=1)
+
+    df["collision_energy"] = parquet_df["collision_energy_aligned_normed"]
+    df["intensities_raw"] = parquet_df["intensities_raw"]
+    df["target"] = df.apply(truncate_target, axis=1)
+    df = df.drop("intensities_raw", axis=1)
+    return df
+
+
+def process_input_df_columns(input_df):
+    input_df.loc[:, "sequence"] = input_df["sequence"].apply(
+        lambda x: np.array(list(x))
+    )
+    input_df.loc[:, "precursor_charge"] = input_df["precursor_charge"].apply(np.array)
+    input_df.loc[:, "collision_energy"] = input_df["collision_energy"].apply(
+        lambda x: [x]
+    )
+    return input_df
+
+
+def df_to_ragged_tensors(df):
+    sequence_col = df["sequence"].tolist()
+    collision_energy_col = df["collision_energy"].tolist()
+    precursor_charge_col = df["precursor_charge"].tolist()
+    target_col = df["target"].tolist()
+
+    sequence_ragged = tf.ragged.constant(sequence_col, dtype=tf.string)
+    collision_energy_ragged = tf.ragged.constant(collision_energy_col, dtype=tf.float32)
+    precursor_charge_ragged = tf.ragged.constant(precursor_charge_col, dtype=tf.float32)
+    target_ragged = tf.ragged.constant(target_col, dtype=tf.float64)
+    return (
+        sequence_ragged,
+        collision_energy_ragged,
+        precursor_charge_ragged,
+        target_ragged,
+    )
+
+
+def ragged_to_tfdataset(
+    sequence_ragged,
+    collision_energy_ragged,
+    precursor_charge_ragged,
+    target_ragged,
+    batch_size=1024,
+):
+    dataset_seq_target = (
+        tf.data.Dataset.from_tensor_slices(
+            (
+                sequence_ragged,
+                target_ragged,
+            )
+        )
+        .map(ragged_to_dense)
+        .padded_batch(batch_size)
+        .unbatch()
+    )
+
+    dataset_meta = tf.data.Dataset.from_tensor_slices(
+        (precursor_charge_ragged, collision_energy_ragged)
+    ).map(ragged_to_dense)
+
+    dataset = (
+        tf.data.Dataset.zip(dataset_seq_target, dataset_meta)
+        .map(merge_tuples)
+        .batch(batch_size)
+        .prefetch(tf.data.AUTOTUNE)
+    )
+    return dataset
+
+
+def get_tfdatasets(batch_size):
+    train_df = pd.read_parquet(DATA_CONFIG["data_source"]["train"])
+    val_df = pd.read_parquet(DATA_CONFIG["data_source"]["val"])
+
+    train_in_df = create_input_df(train_df)
+    val_in_df = create_input_df(val_df)
+
+    train_in_df = process_input_df_columns(train_in_df)
+    val_in_df = process_input_df_columns(val_in_df)
+
+    train_ragged = df_to_ragged_tensors(train_in_df)
+    val_ragged = df_to_ragged_tensors(val_in_df)
+
+    train_dataset = ragged_to_tfdataset(*train_ragged)
+    val_dataset = ragged_to_tfdataset(*val_ragged)
+    return train_dataset, val_dataset
