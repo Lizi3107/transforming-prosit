458d3637aacd8cf786fadd36d17529234100e9a5
Lizi3107 <mamisashvili.lizi@gmail.com>
2023-10-11 18:12:21 +0200

    remove unused layers from variable len models

--- prosit_t/models/variable_seq_length_models.py
+++ prosit_t/models/variable_seq_length_models.py
@@ -13,6 +13,7 @@ class ProstTransformerDynamicLen(tf.keras.Model):
     def __init__(
         self,
         embedding_output_dim=16,
+        meta_embedding_dim=64,
         vocab_dict=ALPHABET_UNMOD,
         len_fion=6,
         dropout_rate=0.2,
@@ -20,29 +21,19 @@ class ProstTransformerDynamicLen(tf.keras.Model):
         ff_dim=32,
         transformer_dropout=0.1,
         num_transformers=2,
-        dense_dim_factor=4,
         **kwargs,
     ):
         super(ProstTransformerDynamicLen, self).__init__()
 
         self.embeddings_count = len(vocab_dict) + 2
-        self.embed = tf.keras.layers.Embedding(
-            input_dim=self.embeddings_count,
-            output_dim=embedding_output_dim,
-            mask_zero=False,
-        )
-        self.flatten = tf.keras.layers.Flatten()
-        self.tdense = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(len_fion))
-
         self.string_lookup = preprocessing.StringLookup(
             vocabulary=list(vocab_dict.keys())
         )
         self.pos_embedding = PositionalEmbedding(
             self.embeddings_count, embedding_output_dim
         )
-        self.meta_encoder = MetaEncoder(
-            embedding_output_dim * dense_dim_factor, dropout_rate
-        )
+        self.meta_encoder = MetaEncoder(meta_embedding_dim, dropout_rate)
+        self.reshape = tf.keras.layers.Reshape([1, meta_embedding_dim])
         self.transformer_encoder = TransformerEncoder(
             embedding_output_dim,
             num_heads,
@@ -50,15 +41,15 @@ class ProstTransformerDynamicLen(tf.keras.Model):
             rate=transformer_dropout,
             num_transformers=num_transformers,
         )
-        self.reshape = tf.keras.layers.Reshape(
-            [1, embedding_output_dim * dense_dim_factor]
-        )
+        self.leaky_relu = tf.keras.layers.LeakyReLU()
         self.cross_att = CrossAttention(
             num_heads=num_heads,
             key_dim=16,
             dropout=0,
         )
         self.drop = tf.keras.layers.Lambda(lambda x: x[:, :-1, :])
+        self.tdense = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(len_fion))
+        self.flatten = tf.keras.layers.Flatten()
 
     def call(self, inputs, **kwargs):
         peptides_in = inputs["sequence"]
@@ -69,7 +60,9 @@ class ProstTransformerDynamicLen(tf.keras.Model):
         x = self.string_lookup(peptides_in)
         x = self.pos_embedding(x)
         x = self.transformer_encoder(x)  # batch, 3, 64
+        x = self.leaky_relu(x)
         x = self.cross_att(x=x, context=encoded_meta)  # batch, 3, 64
+        x = self.leaky_relu(x)
         x = self.drop(x)  # batch, 2, 64
         x = self.tdense(x)  # batch, 2, 6
         x = self.flatten(x)
@@ -80,6 +73,7 @@ class ProstTransformerDynamicLenPooling(tf.keras.Model):
     def __init__(
         self,
         embedding_output_dim=16,
+        meta_embedding_dim=64,
         vocab_dict=ALPHABET_UNMOD,
         len_fion=6,
         dropout_rate=0.2,
@@ -87,17 +81,11 @@ class ProstTransformerDynamicLenPooling(tf.keras.Model):
         ff_dim=32,
         transformer_dropout=0.1,
         num_transformers=2,
-        dense_dim_factor=4,
         **kwargs,
     ):
         super(ProstTransformerDynamicLenPooling, self).__init__()
 
         self.embeddings_count = len(vocab_dict) + 2
-        self.embed = tf.keras.layers.Embedding(
-            input_dim=self.embeddings_count,
-            output_dim=embedding_output_dim,
-            mask_zero=False,
-        )
         self.flatten = tf.keras.layers.Flatten()
         self.tdense = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(len_fion))
 
@@ -107,9 +95,7 @@ class ProstTransformerDynamicLenPooling(tf.keras.Model):
         self.pos_embedding = PositionalEmbedding(
             self.embeddings_count, embedding_output_dim
         )
-        self.meta_encoder = MetaEncoder(
-            embedding_output_dim * dense_dim_factor, dropout_rate
-        )
+        self.meta_encoder = MetaEncoder(meta_embedding_dim, dropout_rate)
         self.transformer_encoder = TransformerEncoder(
             embedding_output_dim,
             num_heads,
@@ -117,9 +103,7 @@ class ProstTransformerDynamicLenPooling(tf.keras.Model):
             rate=transformer_dropout,
             num_transformers=num_transformers,
         )
-        self.reshape = tf.keras.layers.Reshape(
-            [1, embedding_output_dim * dense_dim_factor]
-        )
+        self.reshape = tf.keras.layers.Reshape([1, meta_embedding_dim])
         self.cross_att = CrossAttention(
             num_heads=num_heads,
             key_dim=16,
